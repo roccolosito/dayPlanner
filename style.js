f397972446601887da31f4d83ba42db104dd3773
Rocco Losito <roccolosito@gmail.com>
2020-06-12 11:01:31 -0400

    added screenshots

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    // Retrieving date & time from Moment.JS
+    // Retrieving date & time using Moment.JS
     const today = moment().format('LLLL');
 
     // Code to have date & time displayed on the webpage 
@@ -29,15 +29,72 @@ $(document).ready(function () {
             }
         }
 
-        // Click event for hitting save button & adding user's inputs in to LocalStorage
-        function plansEvent(event) {
-            var userInput = $(this).siblings("input").val().trim();
-            var hourId = $(this).siblings("input").attr("data-index");
+        // Get stored plans from localStorage
+        // Parsing the JSON string to an object
+        let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
 
-            localStorage.setItem(hourId, userInput);
-        }
+        // If plans were retrieved from localStorage, update the plan array to it
+        if (storedPlans !== null) {
+            planTextArr = storedPlans;
+        };
+        // save input to scheduler
+        $('.saveBtn').click(function (e) {
+            event.preventDefault();
+            // console.log('button's clicked');
+            var storedPlans = $(this).val();
+            console.log(storedPlans)
+            var toStore = $(('textarea' + storedPlans)).text();
+            localStorage.setItem(('textarea' + JSON.stringify(storedPlans)), toStore);
+        });
 
-        // $(".saveBtn").on("click", plansEvent)
+        // $('.saveBtn').click(function () {
+        //     event.preventDefault();
+        //     var currentID = $(this).attr(":input")
+        //     var toStore = $(('#textarea', currentID)).val();
+        //     localStorage.setItem(('textarea', currentID), toStore);
+        // });
+
+        // function getPlans() {
+        //     var storedPlans = localStorage.getItem("plans");
+        //     plans = JSON.parse(storedPlans);
+        //     if (!plans) {
+        //         plans = [];
+        //     }
+        // }
+        // getPlans();
+
+        // Function to store locally
+        // function localUser() {
+        //     for (var i = 9; i < 18; i++) {
+        //         $("#" + i).val(localStorage.getItem(i));
+        //     }
+        // }
+        // localUser();
+
+        // // Function to allow user to save their input
+        // var saveBtn = $(".saveBtn");
+        // saveBtn.on("click", function () {
+        //     var pTime = $(this).attr("data-index");
+        //     console.log(pTime);
+        //     var userInput = $("#" + pTime).val();
+        //     console.log(userInput);
+        //     localStorage.setItem(pTime, userInput);
+        // });
+
+
+        //Add Calendar Events to local storage
+        // $('.saveBtn').on('click', 'i', function (event) {
+        //     event.preventDefault();
+
+        //     let index = $(this).attr('save-id');
+
+        //     let inputId = '#input-' + index;
+        //     let value = $(inputId).val();
+
+        //     planTextArr[index] = value;
+
+        // });
+
 
         // Function to auto-refresh page after 5 minutes to ensure time-block
         // colors update every hour without having to manually refresh the page.
